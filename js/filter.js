1ca2ebfd40b04e982633aa9a1fab672dc4d87b7c
Dmitry Buyan <buyandl@mail.ru>
2020-07-23 23:00:02 +0400

    Added filter functionality

--- js/filter.js
+++ js/filter.js
@@ -10,28 +10,68 @@
     TYPE: filterForm.querySelector('#housing-type'),
     PRICE: filterForm.querySelector('#housing-price'),
     ROOMS: filterForm.querySelector('#housing-rooms'),
-    GUESTS: filterForm.querySelector('#housing-guests')
+    GUESTS: filterForm.querySelector('#housing-guests'),
+    FEATURES: filterForm.querySelector('#housing-features')
+  };
+
+  var filterPrice = {
+    low: 10000,
+    high: 50000
   };
 
   var getHouseTypeValue = function (it) {
     return it.offer.type === HouseFilter.TYPE.value || HouseFilter.TYPE.value === 'any';
   };
 
+  var getHousePriceValue = function (it) {
+    switch (HouseFilter.PRICE.value) {
+      case 'low':
+        return it.offer.price < filterPrice.low;
+      case 'middle':
+        return it.offer.price >= filterPrice.low && it.offer.price < filterPrice.high;
+      case 'high':
+        return it.offer.price >= filterPrice.high;
+      default:
+        return true;
+    }
+  };
+
+  var getHouseRoomsValue = function (it) {
+    return it.offer.rooms === Number(HouseFilter.ROOMS.value) || HouseFilter.ROOMS.value === 'any';
+  };
+
+  var getHouseGuestsValue = function (it) {
+    return it.offer.guests === Number(HouseFilter.GUESTS.value) || HouseFilter.GUESTS.value === 'any';
+  };
+
+  var getHouseFeaturesValue = function (it) {
+    var checkedFeatures = Array.from(HouseFilter.FEATURES.querySelectorAll('input:checked'));
+    return checkedFeatures.every(function (feature) {
+      return it.offer.features.includes(feature.value);
+    });
+  };
+
   var filterPins = function (data) {
     var filteredPins = data
     .filter(function (it) {
-      return getHouseTypeValue(it);
+      return (
+        getHouseTypeValue(it) &&
+        getHousePriceValue(it) &&
+        getHouseRoomsValue(it) &&
+        getHouseGuestsValue(it) &&
+        getHouseFeaturesValue(it)
+      );
     })
     .slice(MIN_PINS_COUNT, MAX_PINS_COUNT);
 
     return filteredPins;
   };
 
-  var onHouseTypeChange = function () {
+  var onFormChange = window.debounce(function () {
     window.pin.renderPins(filterPins(window.offers));
-  };
+  });
 
-  HouseFilter.TYPE.addEventListener('change', onHouseTypeChange);
+  filterForm.addEventListener('change', onFormChange);
 
   window.filter = {
     filterPins: filterPins
